53d041055e65236e530b0220d94bdcec5cdfed6c
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-30 11:50:25 +0100

    First commit

---
+++ tut1.php
@@ -0,0 +1,21 @@
+<?php
+    $f_name = "King";
+    $l_name = "MoMo";
+    $age = 22;
+    $height = 1.76;
+    $can_vote = true;
+    $height = 1.76;
+    $height = 1.76;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    
+</body>
+</html>
