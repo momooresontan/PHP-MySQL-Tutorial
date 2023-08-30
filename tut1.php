72d37ea0400eafaecfe168dd1a37bff76278f41c
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-30 14:46:55 +0100

    operators

--- tut1.php
+++ tut1.php
@@ -25,8 +25,30 @@
         <input type="number" name="num1" ><br>
         <label for="state">Number 2: </label>
         <input type="number" name="num2" ><br>
-        <input type="submit">
+        <input type="submit" value="submit">
     </form>
+    <?php 
+        if(isset($_GET) && array_key_exists("state", $_GET)){
+            $state = $_GET["state"];
+            if(isset($state) && !empty($state)){
+                echo "You live in " . $state . "<br>";
+                echo "$f_name lives in $state <br>";
+            }
+            if(count($_GET) >= 3){
+                $num1 = $_GET["num1"];
+                $num2 = $_GET["num2"];
+                echo "$num1 + $num2 = " . ($num1 + $num2) . "<br>";
+                echo "$num1 - $num2 = " . ($num1 - $num2) . "<br>";
+                echo "$num1 * $num2 = " . ($num1 * $num2) . "<br>";
+                echo "$num1 / $num2 = " . ($num1 / $num2) . "<br>";
+                echo "$num1 % $num2 = " . ($num1 % $num2) . "<br>";
+                echo "$num1 / $num2 = " . (intdiv($num1, $num2)) . "<br>";
+
+                echo "increment $num1 = " . ($num1++) . "<br>";
+            }
+        }
+
+    ?>
 
 </body>
 </html>
