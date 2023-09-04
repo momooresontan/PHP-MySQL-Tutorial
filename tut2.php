1bb442e9be904e6100194ab220070acf803cee75
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-04 16:18:29 +0100

    sanitizing html special characters

--- tut2.php
+++ tut2.php
@@ -10,32 +10,40 @@
         <label for="email">Email: </label>
         <input type="text" name="email" ><br>
         <label for="number 1">Number 1: </label>
-        <input type="number" name="num1" ><br>
+        <input type="text" name="num1" ><br>
         <label for="number 2">Number 2: </label>
-        <input type="number" name="num2" ><br>
+        <input type="text" name="num2" ><br>
         <label for="website">Website: </label>
         <input type="text" name="website" ><br>
         <input type="submit" value="submit">
     </form>
     <?php
-        if(isset($_POST["email"])){
-            //INPUT_GET, INPUT_POST, INPUT_COOKIE, INPUT_SERVER, INPUT_ENV
-            if(!filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL)){
-                echo "Email is not valid <br>";
-            }
-        } else{
-            echo "Email is valid <br>";
-        }
-        if(isset($_POST["num1"]) && !empty($_POST["num2"])){
-            $num1 = filter_input(INPUT_POST, "num1", FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-            $num2 = filter_input(INPUT_POST, "num2", FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-            $output = sprintf("%.1f + %.1f = %.1f", $num1, $num2, ($num1 + $num2));
-            echo htmlspecialchars($output) . "<br>";
-            if(isset($_POST["website"])){
-                $website = filter_input(INPUT_POST, "website", FILTER_VALIDATE_URL);
-                echo "Website:" . htmlspecialchars($website) . "<br>";
-            }
-        }
+        //Sanitazation
+        // if(isset($_POST["email"])){
+        //     //INPUT_GET, INPUT_POST, INPUT_COOKIE, INPUT_SERVER, INPUT_ENV
+        //     if(!filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL)){
+        //         echo "Email is not valid <br>";
+        //     }else{
+        //         echo "Email is valid <br>";
+        // } 
+        // }
+        // if(isset($_POST["num1"]) && !empty($_POST["num2"])){
+        //     $num1 = filter_input(INPUT_POST, "num1", FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        //     $num2 = filter_input(INPUT_POST, "num2", FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        //     $output = sprintf("%.1f + %.1f = %.1f", $num1, $num2, ($num1 + $num2));
+        //     echo htmlspecialchars($output) . "<br>";
+        //     if(isset($_POST["website"])){
+        //         $website = filter_input(INPUT_POST, "website", FILTER_VALIDATE_URL);
+        //         echo "Website:" . htmlspecialchars($website) . "<br>";
+        //     }
+        // }
+
+        $con_html = "<a href='#'>Sample</a>";
+        echo $con_html . "<br>" ;
+        echo htmlspecialchars($con_html) . "<br>" ;
+        echo strip_tags($con_html, "<a>") . "<br>";
+        $con_html = strip_tags($con_html) . "<br>";
+        echo $con_html . "<br>";
     ?>
 </body>
 </html>
