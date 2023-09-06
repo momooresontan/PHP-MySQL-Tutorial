cc4db20a7824bbedf7e99efcbb4455259135a4a0
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-06 11:31:18 +0100

    data from db

--- tut3.php
+++ tut3.php
@@ -4,7 +4,7 @@
     $student_statement == $db->prepare($query_students);
     $student_statement ->execute();
     $students = $student_statement->fetchAll();
-       
+    $student_statement->closeCursor();
 ?>
 
 <!DOCTYPE html>
@@ -32,26 +32,15 @@
         <th>Entered</th>
         <th>Lunch</th>
       </tr>
-      <!-- Get an array from the DB query and cycle
-      through each row of data -->
       <?php foreach($students as $student) : ?>
         <tr>
-          <!-- Print out individual column data -->
-          <td><?php echo $student['student_id']; ?></td>
-          <td><?php echo $student['first_name'] . ' ' . $student['last_name']; ?></td>
-          <td><?php echo $student['email']; ?></td>
-          <td><?php echo $student['street']; ?></td>
-          <td><?php echo $student['city']; ?></td>
-          <td><?php echo $student['state']; ?></td>
-          <td><?php echo $student['zip']; ?></td>
-          <td><?php echo $student['phone']; ?></td>
-          <td><?php echo $student['birth_date']; ?></td>
-          <td><?php echo $student['sex']; ?></td>
-          <td><?php echo $student['date_entered']; ?></td>
-          <td><?php echo $student['lunch_cost']; ?></td>
+            <td><?php echo $student["student_id"]; ?></td>
+            <td><?php echo $student["first_name"] . " " . $student["last_name"] ?></td>
+            <td><?php echo $student["email"]; ?></td>
+            <td><?php echo $student["student_id"]; ?></td>
+            <td><?php echo $student["student_id"]; ?></td>
         </tr>
-      <!-- Mark the end of the foreach loop -->
-      <?php endforeach; ?>
+
     </table>
     <h3>Insert Student</h3>
     <form action="add_student.php" method="post"
